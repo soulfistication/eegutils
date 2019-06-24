d22d033bd227c809af83982b5f7ef752b79e512a
Ivan Almada <ivan.almada@me.com>
2019-06-24 14:02:01 -0500

    Initial commit. Generate .csv matrix out of eeg electrode data .dat

---
+++ src/eegutils/Main.java
@@ -0,0 +1,16 @@
+package eegutils;
+
+/**
+ *
+ * @author Ivan Almada
+ */
+public class Main {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        
+    }
+    
+}
